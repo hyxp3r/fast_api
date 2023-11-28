92e00027e188da60820d11a72ebc8f5754b872bb
Evgeniy <erma2001@mail.ru>
2023-11-28 17:50:26 +0700

    ошибка в миграциях 7174a61bb133

--- auth/database.py
+++ auth/database.py
@@ -1,10 +1,17 @@
+from datetime import datetime
 from typing import AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from sqlalchemy import TIMESTAMP, Boolean, ForeignKey, Integer, String
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase 
 from config import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
+
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, declared_attr, mapped_column
+import uuid
+from models.models import Role
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
@@ -14,7 +21,16 @@ class Base(DeclarativeBase):
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
-    pass
+
+    id = mapped_column("id", UUID, primary_key = True, default=uuid.uuid4)
+    username: Mapped[str] = mapped_column("username", String, nullable = False)
+    email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
+    registered_at = mapped_column("registered_at", TIMESTAMP, default=datetime.utcnow)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    role_id = Mapped[UUID] = mapped_column("role.id", Integer, ForeignKey(Role.id))
 
 
 engine = create_async_engine(DATABASE_URL)
--- models/models.py
+++ models/models.py
@@ -1,28 +1,37 @@
 
 from datetime import datetime
 import uuid
-from sqlalchemy import JSON, Integer, Column, String, TIMESTAMP, ForeignKey
+from sqlalchemy import JSON, Boolean, Integer, Column, String, TIMESTAMP, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, declared_attr, mapped_column
 
 Base = declarative_base()
 
 
-class Roles(Base):
-    __tablename__ = "roles"
-    id = Column("id", UUID, primary_key = True, default=uuid.uuid4)
-    name = Column("name", String, nullable = False)
-    permission = Column("permission", JSON)
+class Role(Base):
+
+    __tablename__ = "role"
+
+    id = mapped_column("id", UUID, primary_key = True, default=uuid.uuid4)
+    name = mapped_column("name", String, nullable = False)
+    permission = mapped_column("permission", JSON)
 
 
-class Users(Base):
-    __tablename__ = "users"
-    id = Column("id", UUID, primary_key = True, default=uuid.uuid4)
-    email = Column("email", String, nullable = False)
-    username = Column("username", String, nullable = False)
-    password = Column("password", String, nullable = False)
-    registered_at = Column("registered_at", TIMESTAMP, default=datetime.utcnow)
-    role_id = Column("role_id", Integer, ForeignKey("roles.id"))
+class User(Base):
+
+    __tablename__ = "user"
+
+    id = mapped_column("id", UUID, primary_key = True, default=uuid.uuid4)
+    username: Mapped[str] = mapped_column("username", String, nullable = False)
+    email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
+    registered_at = mapped_column("registered_at", TIMESTAMP, default=datetime.utcnow)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    role_id: Mapped[UUID] = mapped_column("role_id", UUID, ForeignKey(Role.id, ondelete="CASCADE"))
+
 
 
 
