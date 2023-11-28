89c7f869fa29fb9d1eafc9fff5941c283d16f4c3
Evgeniy <erma2001@mail.ru>
2023-11-28 16:51:31 +0700

    поле поменяно на UUID

--- models/models.py
+++ models/models.py
@@ -1,21 +1,23 @@
 
 from datetime import datetime
+import uuid
 from sqlalchemy import JSON, Integer, Column, String, TIMESTAMP, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import UUID
 
 Base = declarative_base()
 
 
 class Roles(Base):
     __tablename__ = "roles"
-    id = Column("id", Integer, primary_key = True)
+    id = Column("id", UUID, primary_key = True, default=uuid.uuid4)
     name = Column("name", String, nullable = False)
     permission = Column("permission", JSON)
 
 
 class Users(Base):
     __tablename__ = "users"
-    id = Column("id", Integer, primary_key = True)
+    id = Column("id", UUID, primary_key = True, default=uuid.uuid4)
     email = Column("email", String, nullable = False)
     username = Column("username", String, nullable = False)
     password = Column("password", String, nullable = False)
